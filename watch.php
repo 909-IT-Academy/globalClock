85ad9e425ae94fcefd85ec0e0072a03a6317c0a3
=dibeshsharma <=dibeshsharma@gmail.com>
2022-04-30 13:25:56 +1000

    layout complete

---
+++ watch.php
@@ -0,0 +1,50 @@
+<!-- 
+Bootstrap 5.1
+Developer : Ashish Gurung 
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aashish  edit</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css?<?=filemtime('css/style.css');?>">
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+
+<div class="container text-uppercase fs-1">
+  <div class="row" style="color:white; min-height:100vh">
+    <div class="col w-50 bg-dark border border-white d-flex align-items-center justify-content-center">
+        <div class="d-flex align-items-center justify-content-center">
+            09<span class="seperator">:</span>25 PM
+        </div>
+    </div>
+    <div class="col w-50 bg-dark">
+        <div class="row border border-white h-50 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center">
+                09<span class="seperator">:</span>25 PM
+            </div>  
+        </div>
+        <div class="row border border-white h-50 ">
+            <div class="d-flex align-items-center justify-content-center">
+                09<span class="seperator">:</span>25 PM
+            </div>  
+        </div>
+    </div>
+  </div>
+</div>    
+</body>
+</html>
+
+
+
+
+
+
+
