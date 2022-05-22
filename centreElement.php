597c850201c8db8efd83c43bc047d1ff9544951f
=dibeshsharma <=dibeshsharma@gmail.com>
2022-05-22 10:22:29 +1000

    centre element

---
+++ centreElement.php
@@ -0,0 +1,29 @@
+<!-- 
+Bootstrap 5.1
+Developer : Dibesh Sharma
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aashish edit</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css?<?= filemtime('css/style.css'); ?>">
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+    <div class="container text-uppercase fs-4 ">
+        <div class="row" style="min-height:100vh">
+            <div class="d-flex justify-content-center align-items-center">
+                <div class="p-2 bd-highlight">Flex item 1</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
